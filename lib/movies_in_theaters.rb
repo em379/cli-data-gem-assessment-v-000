b54cef869842742446b94a2fd72537f3f65909c5
Elaine Miller <emiller7149@gmail.com>
2016-09-07 23:52:38 +0000

    movies_in_theaters

---
+++ lib/movies_in_theaters.rb
@@ -0,0 +1,15 @@
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+require_relative "movies_in_theaters/version"
+require_relative "movies_in_theaters/cli"
+require_relative "movies_in_theaters/movie"
+
+
+module MoviesInTheaters
+  # Your code goes here...
+
+
+end
